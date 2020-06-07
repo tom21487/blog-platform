da242a10c437bf775f0cd52a03060275f9a6afab
Tom Jiao <tom.jiao@outlook.com>
2020-06-07 15:36:32 +0800

    Began implementing tag system.

--- controllers/createController.js
+++ controllers/createController.js
@@ -1,6 +1,7 @@
 var mongo = require('../mongo');
 var db = mongo.getDb();
 var tagsCollection = db.collection('tags');
+var projectsCollection = db.collection('projects');
 
 exports.index = function(req, res, next) {
   tagsCollection.find().toArray(function(err, tags) {
@@ -14,5 +15,15 @@ exports.index = function(req, res, next) {
 
 exports.sendToDb = function(req, res, next) {
   console.log(req.body);
-  res.redirect('/create');
+  let newProject = {
+    title: req.body.title,
+    tags: new Array(req.body.tags),
+    description: req.body.description,
+    imgURL: '/images/1.jpeg',
+    url: '/projects/' + req.body._id
+  }
+  // Project url should really be /projects/formatted-and-escaped-name
+  projectsCollection.insertOne(newProject, function(err, result) {
+    res.redirect('/projects'); // should really redirect to newProject's url
+  });
 }
