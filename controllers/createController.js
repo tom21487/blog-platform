16e926cc7f45ccbbd229f875ba9732811e5b736a
Tom Jiao <tom.jiao@outlook.com>
2020-06-08 15:05:10 +0800

    Fixed tag displaying on project list page.

--- controllers/createController.js
+++ controllers/createController.js
@@ -14,10 +14,16 @@ exports.index = function(req, res, next) {
 }
 
 exports.sendToDb = function(req, res, next) {
-  console.log(req.body);
+  // Manual tags array conversion
+  let tags = req.body.tags;
+  if (!req.body.tags) {
+    tags = new Array("not tagged");
+  } else if (!(req.body.tags instanceof Array)) {
+    tags = new Array(req.body.tags);
+  }
   let newProject = {
     title: req.body.title,
-    tags: new Array(req.body.tags),
+    tags: tags,
     description: req.body.description,
     imgURL: '/images/1.jpeg',
     url: '/projects/' + req.body._id
