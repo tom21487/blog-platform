578bf33ebc451e66572a4dc72b81325b8205c063
Tom Jiao <tom.jiao@outlook.com>
2020-06-15 16:35:17 +0800

    Bilingual v0.4

--- routes/create.js
+++ routes/create.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var router = express.Router();
+var router = express.Router({mergeParams: true});
 
 // Require controller modules
 var createController = require('../controllers/createController.js');
