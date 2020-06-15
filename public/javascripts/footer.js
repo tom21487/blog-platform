9cec9f28d112474a46a71bcc731ab2b9343743cb
Tom Jiao <tom.jiao@outlook.com>
2020-06-15 17:10:45 +0800

    Implemented language switching.

--- public/javascripts/footer.js
+++ public/javascripts/footer.js
@@ -1,3 +1,19 @@
-function alertChinese() {
-  alert("中文翻譯還沒有完成，清耐心等待。");
+let url = window.location.pathname.split('/');
+let currentLanguage = url[1];
+
+/* Change highlighting on load */
+const other = currentLanguage == "en" ? "cn" : "en";
+document.getElementById(currentLanguage + "Btn").classList.add("selected");
+document.getElementById(other + "Btn").classList.remove("selected");
+
+function switchTo(newLanguage) {
+  if (newLanguage !== "en" && newLanguage !== "cn") {
+    alert("undefined language!")
+    return;
+  }
+
+  /* Redirect webpage */
+  url[1] = newLanguage;
+  console.log(url.join('/'));
+  window.location.href = url.join('/');
 }
