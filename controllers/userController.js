1f577d32d52ce90de09916c43d5a21312cb76f8f
Tom Jiao <tom.jiao@outlook.com>
2020-06-29 21:45:20 +0800

    Added basicHTML template.

--- controllers/userController.js
+++ controllers/userController.js
@@ -49,7 +49,7 @@ exports.checkUser = function(req, res, next) {
       if (err) return next(err);
       if (!result) return res.send("invalid password");
       // create token
-      const token = jwt.sign({ _id: user._id }, "secretkey", { expiresIn: "1h"});
+      const token = jwt.sign({ _id: user._id }, "secretkey", { expiresIn: "2d"});
       // store token in browser cookie
       res.cookie("token", token, { httpOnly: true, sameSite: "lax" });
       return res.redirect("/user");
