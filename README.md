d12852c8cf29ffee00fcea83346343186649f7ba
Tom Jiao <tom.jiao@outlook.com>
2020-07-04 15:01:57 +0800

    Revert previous commit, better error handling.

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ Use Promise.all() (you can use this either with promises or with async - it's re
 Asynchronous queries (do this gradually)
 Delete images using fs
 1. Delete post (done)
-2. Error during createController and changeController
+2. Error during createController and changeController (done)
 3. changeController: remove old post images (if replaced)
 Set up .env file to hide mongo uri, jwt secret key
 Add multer to controller file by making controller an array of middleware functions
--- controllers/changeController.js
+++ controllers/changeController.js
@@ -57,7 +57,6 @@ exports.showForm = async function(req, res, next) {
 }
 
 exports.updateInDb = async function(req, res, next) {
-  let imagesToRemove = [];
   try {
     let originalPost = await db.collection(req.params.type + "s").findOne({_id: req.params.id});
 
@@ -88,7 +87,7 @@ exports.updateInDb = async function(req, res, next) {
     let coverImage = "";
     let descriptionEn = "", descriptionCn = "";
     let textIdx = 0, imageIdx = 0, oldIdx = 0;
-    imagesToRemove = originalPost.images;
+    let imagesToRemove = originalPost.images;
 
     for (section of req.body.order) {
       let newBlock = {}
@@ -162,8 +161,8 @@ exports.updateInDb = async function(req, res, next) {
       res.redirect('/control/change');
     }
   } catch(err) {
-    for (image of imagesToRemove) {
-      fs.unlinkSync("public" + image);
+    for (image of req.files) {
+      fs.unlinkSync(image.path);
     }
     return next(err);
   }
